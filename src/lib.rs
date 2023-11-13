9a7aa48b2085d42126b02ed0c1d4248050a33c3f
Julian Martinez <julian_martinez28@outlook.com>
2023-11-13 09:47:06 -0800

    add battle logic

--- src/lib.rs
+++ src/lib.rs
@@ -1,68 +1,235 @@
 #![no_std]
 
-use soroban_sdk::{contract, contractimpl, contracttype, Address, Env};
+use soroban_sdk::{contract, contractimpl, contracttype, vec, Address, Env, String, Symbol, Vec};
 
 #[contracttype]
 #[derive(Clone, Debug, Eq, PartialEq)]
 pub enum DataKey {
     Player(Address),
+    Battle(Symbol),
+    Players,
+    Battles,
 }
 
 #[contracttype]
 #[derive(Clone, Debug, Eq, PartialEq)]
-pub struct State {
-    pub count: u32,
-    pub last_incr: u32,
-    pub last_decr: u32,
+pub struct PlayerStat {
+    pub health: u32,
+    pub attack: u32,
+    pub defense: u32,
+    pub in_battle: bool,
+}
+
+#[contracttype]
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub struct Battle {
+    pub battle_status: u64,
+    pub name: Symbol,
+    pub players: Vec<Address>,
+    pub moves: Vec<u64>,
+    pub winner: Address,
+}
+
+#[derive(Clone, Copy, PartialEq, Eq, Debug)]
+#[repr(u32)]
+pub enum BattleStatus {
+    Pending = 0,
+    Started = 1,
+    Ended = 2,
 }
 
 #[contract]
-pub struct IncrementContract;
+pub struct BattleContract;
 
 #[contractimpl]
-impl IncrementContract {
-    pub fn increment_enum(env: Env, user: Address, incr: u32) -> u32 {
+impl BattleContract {
+    pub fn add_player(env: Env, user: Address) {
+        user.require_auth();
+        env.storage().instance().set(
+            &DataKey::Player(user.clone()),
+            &PlayerStat {
+                health: 100,
+                attack: 10,
+                defense: 10,
+                in_battle: false,
+            },
+        );
+
+        let mut players: Vec<Address> = env
+            .storage()
+            .instance()
+            .get(&DataKey::Players)
+            .unwrap_or(Vec::new(&env));
+        players.push_front(user.clone());
+    }
+
+    pub fn create_battle(env: Env, name: Symbol, user: Address) {
+        user.require_auth();
+        env.storage().instance().set(
+            &DataKey::Battle(name.clone()),
+            &Battle {
+                battle_status: 0,
+                name: name.clone(),
+                players: vec![&env, user.clone()],
+                moves: Vec::new(&env),
+                winner: env.current_contract_address(),
+            },
+        );
+
+        let mut battles: Vec<Symbol> = env
+            .storage()
+            .instance()
+            .get(&DataKey::Battles)
+            .unwrap_or(Vec::new(&env));
+
+        let mut player: PlayerStat = env
+            .storage()
+            .instance()
+            .get(&DataKey::Player(user.clone()))
+            .unwrap_or(PlayerStat {
+                health: 0,
+                attack: 0,
+                defense: 0,
+                in_battle: false,
+            });
+
+        player.in_battle = true;
+        battles.push_front(name.clone());
+    }
+
+    pub fn join_battle(env: Env, name: Symbol, user: Address) {
+        user.require_auth();
+        let mut battle: Battle = env
+            .storage()
+            .instance()
+            .get(&DataKey::Battle(name.clone()))
+            .unwrap();
+
+        let mut player: PlayerStat = env
+            .storage()
+            .instance()
+            .get(&DataKey::Player(user.clone()))
+            .unwrap();
+
+        assert!(battle.battle_status == 0, "Battle already started");
+
+        battle.players.push_front(user.clone());
+        battle.battle_status = 1;
+        player.in_battle = true;
+    }
+
+    pub fn get_battle(env: Env, name: Symbol) -> Battle {
+        env.storage()
+            .instance()
+            .get(&DataKey::Battle(name.clone()))
+            .unwrap()
+    }
+
+    pub fn increase_health(env: Env, user: Address, incr: u32) -> u32 {
         // Get the current count.
-        let mut state = Self::get_enum(env.clone(), user.clone());
+        let mut player_stat = Self::get_player_stats(env.clone(), user.clone());
 
         // Increment the count.
-        state.count += incr;
-        state.last_incr = incr;
+        player_stat.health += incr;
 
         // Save the count.
         env.storage()
             .instance()
-            .set(&DataKey::Player(user.clone()), &state);
+            .set(&DataKey::Player(user.clone()), &player_stat);
 
         // Return the count to the caller.
-        state.count
+        player_stat.health
     }
 
-    pub fn decrement_enum(env: Env, user: Address, decr: u32) -> u32 {
+    pub fn decrease_health(env: Env, user: Address, decr: u32) -> u32 {
         // Get the current count.
-        let mut state = Self::get_enum(env.clone(), user.clone());
+        let mut player_stat = Self::get_player_stats(env.clone(), user.clone());
 
         // Increment the count.
-        state.count -= decr;
-        state.last_decr = decr;
+        player_stat.health -= decr;
 
         // Save the count.
         env.storage()
             .instance()
-            .set(&DataKey::Player(user.clone()), &state);
+            .set(&DataKey::Player(user.clone()), &player_stat);
 
         // Return the count to the caller.
-        state.count
+        player_stat.health
     }
 
-    pub fn get_enum(env: Env, user: Address) -> State {
+    pub fn increase_attack(env: Env, user: Address, incr: u32) -> u32 {
+        // Get the current count.
+        let mut player_stat = Self::get_player_stats(env.clone(), user.clone());
+
+        // Increment the count.
+        player_stat.attack += incr;
+
+        // Save the count.
+        env.storage()
+            .instance()
+            .set(&DataKey::Player(user.clone()), &player_stat);
+
+        // Return the count to the caller.
+        player_stat.attack
+    }
+
+    pub fn decrease_attack(env: Env, user: Address, decr: u32) -> u32 {
+        // Get the current count.
+        let mut player_stat = Self::get_player_stats(env.clone(), user.clone());
+
+        // Increment the count.
+        player_stat.attack -= decr;
+
+        // Save the count.
+        env.storage()
+            .instance()
+            .set(&DataKey::Player(user.clone()), &player_stat);
+
+        // Return the count to the caller.
+        player_stat.attack
+    }
+
+    pub fn increase_defense(env: Env, user: Address, incr: u32) -> u32 {
+        // Get the current count.
+        let mut player_stat = Self::get_player_stats(env.clone(), user.clone());
+
+        // Increment the count.
+        player_stat.defense += incr;
+
+        // Save the count.
+        env.storage()
+            .instance()
+            .set(&DataKey::Player(user.clone()), &player_stat);
+
+        // Return the count to the caller.
+        player_stat.defense
+    }
+
+    pub fn decrease_defense(env: Env, user: Address, decr: u32) -> u32 {
+        // Get the current count.
+        let mut player_stat = Self::get_player_stats(env.clone(), user.clone());
+
+        // Increment the count.
+        player_stat.defense -= decr;
+
+        // Save the count.
+        env.storage()
+            .instance()
+            .set(&DataKey::Player(user.clone()), &player_stat);
+
+        // Return the count to the caller.
+        player_stat.defense
+    }
+
+    pub fn get_player_stats(env: Env, user: Address) -> PlayerStat {
         env.storage()
             .instance()
             .get(&DataKey::Player(user))
-            .unwrap_or(State {
-                count: 0,
-                last_incr: 0,
-                last_decr: 0,
+            .unwrap_or(PlayerStat {
+                health: 0,
+                attack: 0,
+                defense: 0,
+                in_battle: false,
             })
     }
 }
--- src/test.rs
+++ src/test.rs
@@ -6,50 +6,54 @@ use soroban_sdk::{testutils::Address as _, Address, Env};
 #[test]
 fn test_enum() {
     let env = Env::default();
-    let contract_id = env.register_contract(None, IncrementContract);
-    let client = IncrementContractClient::new(&env, &contract_id);
+    let contract_id = env.register_contract(None, BattleContract);
+    let client = BattleContractClient::new(&env, &contract_id);
     let user_1 = Address::random(&env);
     let user_2 = Address::random(&env);
 
     // User 1 increment series
-    assert_eq!(client.increment_enum(&user_1, &1), 1);
+    assert_eq!(client.increase_health(&user_1, &1), 1);
     assert_eq!(
-        client.get_enum(&user_1),
-        State {
-            count: 1,
-            last_incr: 1,
-            last_decr: 0,
+        client.get_player_stats(&user_1),
+        PlayerStat {
+            health: 1,
+            attack: 0,
+            defense: 0,
+            in_battle: false,
         }
     );
 
-    assert_eq!(client.increment_enum(&user_1, &2), 3);
+    assert_eq!(client.increase_health(&user_1, &2), 3);
     assert_eq!(
-        client.get_enum(&user_1),
-        State {
-            count: 3,
-            last_incr: 2,
-            last_decr: 0,
+        client.get_player_stats(&user_1),
+        PlayerStat {
+            health: 3,
+            attack: 0,
+            defense: 0,
+            in_battle: false,
         }
     );
 
     // User 2 increment series
-    assert_eq!(client.increment_enum(&user_2, &10), 10);
+    assert_eq!(client.increase_health(&user_2, &10), 10);
     assert_eq!(
-        client.get_enum(&user_2),
-        State {
-            count: 10,
-            last_incr: 10,
-            last_decr: 0,
+        client.get_player_stats(&user_2),
+        PlayerStat {
+            health: 10,
+            attack: 0,
+            defense: 0,
+            in_battle: false,
         }
     );
 
-    assert_eq!(client.increment_enum(&user_2, &10), 20);
+    assert_eq!(client.increase_health(&user_2, &10), 20);
     assert_eq!(
-        client.get_enum(&user_2),
-        State {
-            count: 20,
-            last_incr: 10,
-            last_decr: 0,
+        client.get_player_stats(&user_2),
+        PlayerStat {
+            health: 20,
+            attack: 0,
+            defense: 0,
+            in_battle: false,
         }
     );
 }
@@ -57,27 +61,85 @@ fn test_enum() {
 #[test]
 fn test_decrement() {
     let env = Env::default();
-    let contract_id = env.register_contract(None, IncrementContract);
-    let client = IncrementContractClient::new(&env, &contract_id);
+    let contract_id = env.register_contract(None, BattleContract);
+    let client = BattleContractClient::new(&env, &contract_id);
     let user_1 = Address::random(&env);
 
-    assert_eq!(client.increment_enum(&user_1, &10), 10);
+    assert_eq!(client.increase_health(&user_1, &10), 10);
     assert_eq!(
-        client.get_enum(&user_1),
-        State {
-            count: 10,
-            last_incr: 10,
-            last_decr: 0,
+        client.get_player_stats(&user_1),
+        PlayerStat {
+            health: 10,
+            attack: 0,
+            defense: 0,
+            in_battle: false,
         }
     );
 
-    assert_eq!(client.decrement_enum(&user_1, &9), 1);
+    assert_eq!(client.decrease_health(&user_1, &9), 1);
     assert_eq!(
-        client.get_enum(&user_1),
-        State {
-            count: 1,
-            last_incr: 10,
-            last_decr: 9,
+        client.get_player_stats(&user_1),
+        PlayerStat {
+            health: 1,
+            attack: 0,
+            defense: 0,
+            in_battle: false,
         }
     );
+}
+
+#[test]
+fn test_create_and_join_battle() {
+    let env = Env::default();
+    env.mock_all_auths();
+    let contract_id = env.register_contract(None, BattleContract);
+    let client = BattleContractClient::new(&env, &contract_id);
+    let battle_name = Symbol::new(&env, "Constantine");
+    let user_1 = Address::random(&env);
+    let user_2 = Address::random(&env);
+
+    client.create_battle(&battle_name, &user_1);
+    assert_eq!(
+        client.get_battle(&battle_name),
+        Battle {
+            battle_status: 0,
+            name: battle_name.clone(),
+            players: vec![&env, user_1.clone()],
+            moves: Vec::new(&env),
+            winner: contract_id.clone(),
+        }
+    );
+
+    // assert_eq!(client.increase_health(&user_2, &10), 10);
+    // assert_eq!(
+    //     client.get_player_stats(&user_2),
+    //     PlayerStat {
+    //         health: 10,
+    //         attack: 0,
+    //         defense: 0,
+    //         in_battle: false,
+    //     }
+    // );
+
+    // assert_eq!(client.decrease_health(&user_1, &9), 1);
+    // assert_eq!(
+    //     client.get_player_stats(&user_1),
+    //     PlayerStat {
+    //         health: 1,
+    //         attack: 0,
+    //         defense: 0,
+    //         in_battle: false,
+    //     }
+    // );
+
+    // assert_eq!(client.decrease_health(&user_2, &9), 1);
+    // assert_eq!(
+    //     client.get_player_stats(&user_2),
+    //     PlayerStat {
+    //         health: 1,
+    //         attack: 0,
+    //         defense: 0,
+    //         in_battle: false,
+    //     }
+    // );
 }
