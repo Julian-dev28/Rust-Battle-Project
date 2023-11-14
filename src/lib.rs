d94a9173e4916ff922a2d94e8445af54292377f7
Julian Martinez <julian_martinez28@outlook.com>
2023-11-14 10:59:46 -0800

    Add maps for Players and Moves ; Todo: fix errors from change

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
 #![no_std]
 
-use soroban_sdk::{contract, contractimpl, contracttype, vec, Address, Env, String, Symbol, Vec};
+use soroban_sdk::{
+    contract, contractimpl, contracttype, map, vec, Address, Env, Map, String, Symbol, Vec,
+};
 
 #[contracttype]
 #[derive(Clone, Debug, Eq, PartialEq)]
@@ -26,8 +28,8 @@ pub struct PlayerStat {
 pub struct Battle {
     pub battle_status: u64,
     pub name: Symbol,
-    pub players: Vec<Address>,
-    pub moves: Vec<u64>,
+    pub players: Map<Address, u64>,
+    pub moves: Map<Address, u64>,
     pub winner: Address,
 }
 
@@ -94,14 +96,15 @@ impl BattleContract {
 
     pub fn create_battle(env: Env, name: Symbol, user: Address) {
         user.require_auth();
+        let contract_id = env.current_contract_address();
         env.storage().instance().set(
             &DataKey::Battle(name.clone()),
             &Battle {
                 battle_status: 0,
                 name: name.clone(),
-                players: vec![&env, user.clone()],
-                moves: Vec::new(&env),
-                winner: env.current_contract_address(),
+                players: map![&env, (user.clone(), 1), (contract_id.clone(), 2)],
+                moves: map![&env, (user.clone(), 0), (contract_id.clone(), 0)],
+                winner: contract_id.clone(),
             },
         );
 
@@ -123,8 +126,8 @@ impl BattleContract {
             &Battle {
                 battle_status: 1,
                 name: name.clone(),
-                players: vec![&env, user.clone(), contract_id.clone()],
-                moves: Vec::new(&env),
+                players: map![&env, (user.clone(), 1), (contract_id.clone(), 2)],
+                moves: map![&env, (user.clone(), 0), (contract_id.clone(), 0)],
                 winner: contract_id.clone(),
             },
         );
@@ -136,12 +139,16 @@ impl BattleContract {
 
     pub fn join_battle(env: Env, name: Symbol, user: Address) {
         user.require_auth();
+        let contract_id = env.current_contract_address();
         let mut battle = Self::get_battle(env.clone(), name.clone());
         assert!(battle.battle_status == 0, "Battle already started");
         let mut player = Self::get_player_stats(env.clone(), user.clone());
         assert!(!player.in_battle, "Player already in battle");
 
-        battle.players.push_back(user.clone());
+        let mut players = battle.players.clone();
+        players.get(user.clone()).unwrap_or(0);
+        let player_1 = players.keys().get(0).unwrap_or(contract_id.clone());
+        players = map![&env, (player_1, 1), (user.clone(), 2)];
         battle.battle_status = 1;
         player.in_battle = true;
 
@@ -149,26 +156,27 @@ impl BattleContract {
         Self::set_player_stats(env.clone(), user.clone(), player);
     }
 
-    pub fn challenge_bot(env: Env, name: Symbol) {
+    pub fn challenge_bot(env: Env, user: Address, name: Symbol) {
         let mut battle = Self::get_battle(env.clone(), name.clone());
         assert!(battle.battle_status == 0, "Battle already started");
         let contract_id = env.current_contract_address();
 
-        battle.players.push_back(contract_id.clone());
+        battle.players = map![&env, (user.clone(), 1), (contract_id.clone(), 2)];
         battle.battle_status = 1;
 
         Self::set_battle(env.clone(), name.clone(), battle);
     }
 
     pub fn get_battle(env: Env, name: Symbol) -> Battle {
+        let contract_id = env.current_contract_address();
         env.storage()
             .instance()
             .get(&DataKey::Battle(name.clone()))
             .unwrap_or(Battle {
                 battle_status: 0,
                 name: name.clone(),
-                players: Vec::new(&env),
-                moves: Vec::new(&env),
+                players: map![&env, (1, contract_id.clone()), (2, contract_id.clone())],
+                moves: map![&env, (contract_id.clone(), 0), (contract_id.clone(), 0)],
                 winner: env.current_contract_address(),
             })
     }
@@ -273,6 +281,74 @@ impl BattleContract {
         // Return the count to the caller.
         player_stat.defense
     }
+
+    pub fn attack_or_defend_choice(env: Env, user: Address, choice: u64, battle_name: Symbol) {
+        user.require_auth();
+        let contract_id = env.current_contract_address();
+        let battle = Self::get_battle(env.clone(), battle_name.clone());
+
+        assert!(
+            battle.battle_status == 1,
+            "Battle not started. Please tell another player to join the battle"
+        ); // Require that battle has started
+        assert!(battle.battle_status != 2, "Battle has already ended"); // Require that battle has not ended
+        assert!(
+            battle.players.contains_key(user.clone()),
+            "You are not in this battle"
+        ); // Require that player is in the battle
+
+        Self::register_player_move(env.clone(), user.clone(), choice, battle_name.clone());
+
+        let battle = Self::get_battle(env.clone(), battle_name.clone());
+        let moves_left = 2
+            - (battle.moves.get(user.clone()).unwrap_or(0) == 0) as u64
+            - (battle.moves.get(contract_id.clone()).unwrap_or(0) == 0) as u64;
+
+        // if (moves_left == 0) {
+        //     Self::await_battle_results(env.clone(), battle_name.clone());
+        // }
+    }
+
+    fn register_player_move(
+        env: Env,
+        user: Address,
+        choice: u64,
+        battle_name: Symbol,
+    ) -> Result<(), String> {
+        let mut battle = Self::get_battle(env.clone(), battle_name.clone());
+        Map::set(&mut battle.moves, user.clone(), choice);
+        Self::set_battle(env.clone(), battle_name.clone(), battle);
+        Ok(())
+    }
+
+    //     function attackOrDefendChoice(uint8 _choice, string memory _battleName) external {
+    //     Battle memory _battle = getBattle(_battleName);
+
+    //     require(
+    //         _battle.battleStatus == BattleStatus.STARTED,
+    //         "Battle not started. Please tell another player to join the battle"
+    //     ); // Require that battle has started
+    //     require(
+    //         _battle.battleStatus != BattleStatus.ENDED,
+    //         "Battle has already ended"
+    //     ); // Require that battle has not ended
+    //     require(
+    //       msg.sender == _battle.players[0] || msg.sender == _battle.players[1],
+    //       "You are not in this battle"
+    //     ); // Require that player is in the battle
+
+    //     require(_battle.moves[_battle.players[0] == msg.sender ? 0 : 1] == 0, "You have already made a move!");
+
+    //     _registerPlayerMove(_battle.players[0] == msg.sender ? 0 : 1, _choice, _battleName);
+
+    //     _battle = getBattle(_battleName);
+    //     uint _movesLeft = 2 - (_battle.moves[0] == 0 ? 0 : 1) - (_battle.moves[1] == 0 ? 0 : 1);
+    //     emit BattleMove(_battleName, _movesLeft == 1 ? true : false);
+
+    //     if(_movesLeft == 0) {
+    //       _awaitBattleResults(_battleName);
+    //     }
+    //   }
 }
 
 #[cfg(test)]
